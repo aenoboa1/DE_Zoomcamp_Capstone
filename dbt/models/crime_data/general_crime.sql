549d6023ef494b5bfe2b6cc5d528db1cb5e7f198
Andres Noboa V <56968012+aenoboa1@users.noreply.github.com>
2022-05-02 16:04:41 -0500

    Update general_crime.sql

--- dbt/models/crime_data/general_crime.sql
+++ dbt/models/crime_data/general_crime.sql
@@ -31,37 +31,4 @@ SELECT
     location, 
 
 
-from {{ref('crime_stage')}}{{config(materialized = 'view',
-
-    partition_by = {
-      "field": "crime_date",
-      "data_type": "timestamp",
-      "granularity": "month"
-})}}
-
-SELECT
-    id,
-    case_number,
-    crime_date,
-    block,
-    iucr,
-    primary_type,
-    description,
-    location_description,
-    arrest, 
-    domestic, 
-    beat_incident,
-    crime_district,
-    crime_ward ,
-    community_area,
-    fbi_code,  
-    x_coor, 
-    y_coor,
-    year,
-    upload_date,
-    latitude,  
-    longitude,
-    location, 
-
-
 from {{ref('crime_stage')}}
